229b7b3354c4b61673a3ec5d2f8d1ecb69eb8dda
admslts <152650492+admslts@users.noreply.github.com>
2024-01-08 21:17:15 +0100

    Update loginVer.js

--- JS/loginVer.js
+++ JS/loginVer.js
@@ -1,8 +1,9 @@
-    const isLoggedIn = sessionStorage.getItem('loggedIn');
-    setInterval(logOut, 5000);
     function logOut() {
         window.location.href = "file:///Users/adam/Desktop/UniMap/Unimap";
     }
+
+    const isLoggedIn = sessionStorage.getItem('loggedIn');
+    setInterval(logOut, 5000);
 
     if (!isLoggedIn) {
         window.location.href = "index.html";
