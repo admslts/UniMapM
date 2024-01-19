dad55de910c73a6a79b3bed755c7e2526f3fec50
admslts <152650492+admslts@users.noreply.github.com>
2024-01-19 19:48:25 +0100

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -6,7 +6,7 @@
         if (urlToken == authToken) {
             sessionStorage.setItem('loggedIn', 'true');
             document.getElementById('login-status').textContent = 'Prihlásenie úspešné';
-            window.location.href = "https://admslts.github.io/UniMapM/mapa.html";
+            window.location.href = "unimap.sk/?token=bHdl106R0dlBJnA5XWjA";
             }
         else {
             document.getElementById("login-status").textContent = "Naskenujte QR kód svojim telefónom";
